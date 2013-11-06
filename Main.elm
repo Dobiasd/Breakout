9b90a6a48e42080c01bfc3ddd632ec50a65c26db
Dobiasd <harry@daiw.de>
2013-11-06 12:49:26 +0100

    comments

--- Main.elm
+++ Main.elm
@@ -1,25 +1,41 @@
 module Breakout where
 
+{-| A simple clone of the classic arcade game Breakout.
+
+Destroy all bricks with the ball, moving the paddle my mouse or touch.
+The number of spare balls is restricted. Every lost ball and every
+paddle touch cost points. Every hit brick adds points.
+
+To add horizontal speed to the ball, move the paddle while serving/hitting.
+-}
+
 import Keyboard
 import String
 import Touch
 import Window
 
 
--- model configuration
+-- /---------------------\
+-- | model configuration |
+-- \---------------------/
 
+{-| The game field extends from -300 to +300 in x coordinates
+                       and from -200 to +200 in y coordinates. -}
+(gameWidth,gameHeight) = (600,400)
+(halfWidth,halfHeight) = (toFloat gameWidth / 2, toFloat gameHeight / 2)
+
+{-| We aim for a maximally smooth gameplay. -}
 framesPerSecond = 60
+
 paddleSpeed = 110
 paddleWidths = 52
 paddleHeight = 4
-brake = 0.7
-traction = 0.55
-serveSpeed = 200
-speedIncX = 1.01
-speedIncY = 1.02
-paddleYPos = 40 - gameHeight/2
-(gameWidth,gameHeight) = (600,400)
-(halfWidth,halfHeight) = (toFloat gameWidth / 2, toFloat gameHeight / 2)
+brake = 0.7 -- If no input is present, the paddle will slow down.
+traction = 0.55 -- How much does the paddle speed influence the ball speed?
+serveSpeed = 200 -- initial y speed of the ball
+speedIncX = 1.01 -- the ball speeds up during play
+speedIncY = 1.02 -- in both directions
+paddleYPos = 40 - gameHeight/2 -- paddle is near the screens bottom
 brickDistX = 80
 brickDistY = 33
 brickWidth = 50
@@ -34,24 +50,29 @@ pointsPerContact = -1
 speedFactor = 1
 
 
--- view configuration
+-- /--------------------\
+-- | view configuration |
+-- \--------------------/
 
 manualMsg = "SPACE to serve, &larr; and &rarr; to move;" ++
             " or just touch the quadrants"
 wonMsg = "Congratulations! Serve to restart."
 lostMsg = "Serve to restart. ;)"
-breakoutBlue = rgb 60 60 100
-textBlue = rgb 160 160 200
 brickColorFactor = 0.01
 endTextHeight = 24
 msgTextPosY = 20 - halfHeight
 pointsTextPos = (64 - halfWidth, halfHeight - 10)
 spareBallsTextPos = (halfWidth - 69, halfHeight - 10)
-quadrantCol = rgba 0 0 0 0.4
+breakoutBlue = rgb   60  60 100
+textBlue     = rgb  160 160 200
+quadrantCol  = rgba   0   0   0 0.4
 
 
--- Inputs
+-- /--------\
+-- | inputs |
+-- \--------/
 
+{-| Check if the user touched one of the four screen quadrants. -}
 touchInQuadrant : Int -> (Int,Int) -> Touch.Touch -> Maybe Bool
 touchInQuadrant q (w,h) touch =
   let
@@ -78,9 +99,11 @@ touchLowerLeft = (.) (maybe False id) . touchInQuadrant 3
 touchLowerRight : (Int,Int) -> Touch.Touch -> Bool
 touchLowerRight = (.) (maybe False id) . touchInQuadrant 4
 
+{-| Was the upper half of the screen touched? -}
 touchUpper : (Int,Int) -> Touch.Touch -> Bool
 touchUpper (w,h) t = touchUpperLeft (w,h) t || touchUpperRight (w,h) t
 
+{-| Touching the upper quadrant can be used to serve like the space key. -}
 spaceSignal : Signal Bool
 spaceSignal =
   let
@@ -88,6 +111,8 @@ spaceSignal =
   in
     lift3 f Keyboard.space Touch.touches Window.dimensions
 
+{-| The paddle can be moved with the arrow keys
+or by touching the lower quadrants. -}
 dirSignal : Signal Int
 dirSignal =
   let
@@ -100,10 +125,15 @@ dirSignal =
   in
     lift3 f Keyboard.arrows Touch.touches Window.dimensions
 
-type Input = { space:Bool, dir:Int, delta:Time }
-
+{-| Game speed can be adjusted. -}
 delta : Signal Float
 delta = lift (\d -> speedFactor * d) <| inSeconds <~ fps framesPerSecond
+
+{-| Relevant things that can change are:
+- did the user serve the ball?
+- input direction of the paddle
+- time delta between this and the last frame. -}
+type Input = { space:Bool, dir:Int, delta:Time }
 
 input : Signal Input
 input = sampleOn delta (Input <~ spaceSignal
@@ -111,19 +141,21 @@ input = sampleOn delta (Input <~ spaceSignal
                                ~ delta)
 
 
--- Model
-
-data State = Play | Serve | Won | Lost
+-- /-------\
+-- | model |
+-- \-------/
 
 type Positioned a = { a | x:Float, y:Float }
 type Moving     a = { a | vx:Float, vy:Float }
 type Sized      a = { a | w:Float, h:Float }
 
 type Box = Sized (Positioned {})
-
 type Brick = Box
+
 type Ball = Moving (Positioned { r:Float })
-type Player = Sized (Moving (Positioned {}))
+type Player = Moving Box
+
+data State = Play | Serve | Won | Lost
 
 ball : Float -> Float -> Float -> Float -> Float -> Ball
 ball x y vx vy r = {x=x, y=y, vx=vx, vy=vy, r=r }
@@ -134,6 +166,7 @@ player x y vx vy w h = {x=x, y=y, vx=vx, vy=vy, w=w, h=h }
 brick : Float -> Float -> Float -> Float -> Brick
 brick x y w h = {x=x, y=y, w=w, h=h }
 
+{-| Creation of one single row of bricks with equidistant gaps. -}
 brickRow : Float -> [Brick]
 brickRow y =
   let xOff = toFloat (ceiling  (-brickCols / 2)) * brickDistX
@@ -143,10 +176,15 @@ brickRow y =
 type Game = { state:State
             , gameBall:Ball
             , player:Player
+
+-- The bricks still left in the game.
             , bricks:[Brick]
+
+-- How many balls are left? (excluding the one currently played)
             , spareBalls:Int
-            , contacts:Int
-            }
+
+-- Count the number of contacts of the paddle with a ball.
+            , contacts:Int }
 
 defaultGame : Game
 defaultGame =
@@ -160,29 +198,45 @@ defaultGame =
   }
 
 
--- Updates
+-- /---------\
+-- | updates |
+-- \---------/
 
+{-| Move an object according to its speed for a given time step t. -}
 stepObj : Float -> Moving (Positioned a) -> Moving (Positioned a)
 stepObj t ({x,y,vx,vy} as obj) =
     { obj | x <- x + vx*t, y <- y + vy*t }
 
+{-| Is the distance between n and k less or equal c? -}
 near : number -> number -> number -> Bool
 near k c n = n >= k-c && n <= k+c
 
+{-| I the ball overlapping the box? -}
 within : Ball -> Sized (Positioned a) -> Bool
 within ball box = (ball.x |> near box.x (ball.r + box.w / 2))
                && (ball.y |> near box.y (ball.r + box.h / 2))
 
+{-| Keep an object with speed v inside its lower and upper bounds. -}
 stepV : Float -> Bool -> Bool -> Float
 stepV v lowerCollision upperCollision =
   if | lowerCollision -> abs v
      | upperCollision -> 0 - abs v
      | otherwise      -> v
 
-speedUp : Ball -> Ball
-speedUp ({vx, vy} as ball) = {ball | vx <- speedIncX * vx
-                                   , vy <- speedIncY * vy }
+{-| Increate the speed of a moving object. -}
+speedUp : Moving a -> Moving a
+speedUp ({vx, vy} as obj) = {obj | vx <- speedIncX * vx
+                                 , vy <- speedIncY * vy }
 
+{-| Simple weighted arithmetic mean. -}
+weightedAvg : [number] -> [number] -> number
+weightedAvg values weights =
+  let
+    weightedVals = zipWith (*) values weights
+  in
+    sum weightedVals / sum weights
+
+{-| foldr function for ball brick collisions. -}
 goBrickHits : Brick -> (Ball,[Brick]) -> (Ball,[Brick])
 goBrickHits brick (ball,bricks) =
   let
@@ -192,13 +246,10 @@ goBrickHits brick (ball,bricks) =
   in
     (if hit then speedUp ball' else ball', bricks')
 
-weightedAvg : [number] -> [number] -> number
-weightedAvg values weights =
-  let
-    weightedVals = zipWith (*) values weights
-  in
-    sum weightedVals / sum weights
-
+{-| Collision handling of the ball with the paddle and the bricks
+during a given timestep.
+Returns the new ball properties, the bricks left and perhaps increased
+count of paddle ball contact. -}
 stepBall : Time -> Ball -> Player -> [Brick] -> Int -> ((Ball,[Brick]), Int)
 stepBall t ({x,y,vx,vy} as ball) p bricks contacts =
   let
@@ -213,12 +264,13 @@ stepBall t ({x,y,vx,vy} as ball) p bricks contacts =
   in
     (foldr goBrickHits (ball1,[]) bricks, contacts')
 
-
-stepPlyr : Time -> Int -> Player -> Player
-stepPlyr t dir p =
+{-| Calculate how the players properties have changed. -}
+stepPlayer : Time -> Int -> Player -> Player
+stepPlayer t dir p =
   let p1 = stepObj t { p | vx <- p.vx * brake + toFloat dir * paddleSpeed }
   in  { p1 | x <- clamp (p.w/2-halfWidth) (halfWidth-p.w/2) p1.x }
 
+{-| Which state falls the game into regarding its current situation? -}
 nextState : Bool -> Game -> (State, Int)
 nextState space ({state,gameBall,bricks,spareBalls} as game) =
   let
@@ -234,6 +286,7 @@ nextState space ({state,gameBall,bricks,spareBalls} as game) =
   in
     (state', max 0 spareBalls')
 
+{-| Advance the game according to its current conditions and the input. -}
 stepGame : Input -> Game -> Game
 stepGame {space,dir,delta}
          ({state,gameBall,player,bricks,spareBalls,contacts} as game) =
@@ -252,7 +305,7 @@ stepGame {space,dir,delta}
     if finished && space then defaultGame else
       { game | state      <- state'
              , gameBall   <- ball'
-             , player     <- stepPlyr delta dir player
+             , player     <- stepPlayer delta dir player
              , bricks     <- bricks'
              , spareBalls <- spareBalls'
              , contacts   <- if finished then contacts else contacts' }
@@ -261,21 +314,28 @@ gameState : Signal Game
 gameState = foldp stepGame defaultGame input
 
 
--- Display
+-- /---------\
+-- | display |
+-- \---------/
 
+{-| Render text using a given transformation function. -}
 txt : (Text -> Text) -> String -> Element
 txt f = text . f . monospace . Text.color textBlue . toText
 
+{-| Take a shape, give it a color and move it to the objects position. -}
 make : Color -> Positioned a -> Shape -> Form
 make color obj shape = shape |> filled color
                              |> move (obj.x,obj.y)
 
+{-| Generate the rainbow color of a brick depending on its position. -}
 brickColor : Brick -> Color
 brickColor b = hsv (brickColorFactor * (b.x + b.y)) 1 1
 
+{-| Dummy for cases in which an game object or text should be invisible. -}
 noForm : Form
 noForm = rect 0 0 |> filled (rgba 0 0 0 0)
 
+{-| Draw the touch screen quadrants required for controlling the game. -}
 displayQuadrants : (Float,Float) -> State -> Form
 displayQuadrants (w,h) state =
   let
@@ -286,8 +346,10 @@ displayQuadrants (w,h) state =
   in
     if state == Serve then grid else noForm
 
-pointsText : Int -> Int -> Int -> String
-pointsText bricksLeft spareBalls contacts =
+{-| How many points does the player have with his current game statistics,
+and how many can he achieve maximally in a game? -}
+calcPoints : Int -> Int -> Int -> (Int,Int)
+calcPoints bricksLeft spareBalls contacts =
   let
     maxBricks = brickRows * brickCols
     maxPoints = pointsPerBrick * maxBricks
@@ -295,10 +357,19 @@ pointsText bricksLeft spareBalls contacts =
     points = pointsPerBrick * bricksGone +
              pointsPerBall * (startSpareBalls - spareBalls) +
              pointsPerContact * contacts
+  in
+    (points,maxPoints)
+
+{-| Generate the String showing the number of curent points. -}
+pointsText : Int -> Int -> Int -> String
+pointsText bricksLeft spareBalls contacts =
+  let
+    (points,maxPoints) = calcPoints bricksLeft spareBalls contacts
     maxPointsStrLen = String.length <| show maxPoints
   in
     "points: " ++ (String.padLeft maxPointsStrLen ' ' <| show points)
 
+{-| Draw game into a form with size (gameWidth,gameHeight). -}
 display : Game -> Form
 display {state,gameBall,player,bricks,spareBalls,contacts} =
   let
@@ -335,6 +406,7 @@ display {state,gameBall,player,bricks,spareBalls,contacts} =
       , quadrants
       ]
 
+{-| Draw game maximized into the window. -}
 displayFullScreen : (Int,Int) -> Game -> Element
 displayFullScreen (w,h) game =
   let
