2506e54623612ba5811d51e9307d1fd98982e4a1
Dobiasd <harry@daiw.de>
2014-01-05 13:54:38 +0100

    new Elm version

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 elm -m --runtime=elm-runtime.js Main.elm
-cp $HOME/.cabal/share/Elm-0.10/elm-runtime.js ./build
+cp $HOME/.cabal/share/Elm-0.10.1/elm-runtime.js ./build
 mv ./build/Main.html ./build/index.html
